186149dce24db468121f5dd26ee9fbfcda8d44f7
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-02-28 07:06:52 -0500

    String Lesson Completed

--- Script.py
+++ Script.py
@@ -79,3 +79,64 @@ print(password)
 
 #Iterating through Strings
 
+def get_length(word):
+  counter = 0
+  for letter in word:
+    counter += 1
+  return counter
+
+
+city_name = "Medellin"
+
+function_getlength = get_length(city_name)
+
+print(function_getlength)
+
+#Strings and Conditionals
+
+
+def letter_check(word, letter):
+  for character in word:
+    if character == letter:
+      return True
+  return False
+
+
+brand = "nintendo"
+letter_to_check = "n"
+
+letter_check_function = letter_check(brand, letter_to_check)
+print(letter_check_function)
+
+
+def contains(big_string, little_string):
+  return little_string in big_string
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
+
+
+
+#Review
+
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+
+
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i - 1]
+    return password
