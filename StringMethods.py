bcb09f6ee2d575164a19a471abe28a4b928e3684
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-02 07:10:34 -0500

    String Methods Notes Completed

--- StringMethods.py
+++ StringMethods.py
@@ -114,3 +114,51 @@ print(disown_placement)
 # than it is to picture the end result of string concatenation and legibility is everything. You can also reuse the same base
 # string with different variables, allowing you to cut down on unnecessary, hard to interpret code.
 
+def poem_title_card(title, poet):
+  poem_desc = "The poem \"{}\" is written by {}.".format(title, poet)
+  return poem_desc
+
+
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date = publishing_date, author = author, title = title , original_work = original_work)
+  return poem_desc
+
+my_beard_description = poem_description(author = "Shel Silverstein", title = "My Beard", original_work = "Where the Sidewalk Ends", publishing_date = "1974")
+
+
+print(my_beard_description)
+
+
+#Review
+
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+# print(highlighted_poems)
+
+highlighted_poems_list = highlighted_poems.split(',')
+
+# print(highlighted_poems_list)
+
+highlighted_poems_stripped = []
+
+for poem in highlighted_poems_list:
+    highlighted_poems_stripped.append(poem.strip())
+
+# print(highlighted_poems_stripped)
+
+highlighted_poems_details = []
+
+for poem in highlighted_poems_stripped:
+    highlighted_poems_details.append(poem.split(':'))
+
+titles = []
+poets = []
+dates = []
+
+for poem in highlighted_poems_details:
+    titles.append(poem[0])
+    poets.append(poem[1])
+    dates.append(poem[2])
+
+for i in range(0, len(highlighted_poems_details)):
+    print('The poem {} was published by {} in {}'.format(titles[i], poets[i], dates[i]))
